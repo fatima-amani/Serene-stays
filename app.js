23657f3e0e73eb6fc1925b700437daaf5e550aa8
fatima-amani <1rn21ai048.fatimaamani@gmail.com>
2025-04-25 21:25:18 +0530

    add redirect from root to listings and update package name to 'serenestays'

--- app.js
+++ app.js
@@ -88,6 +88,10 @@ app.use( (req,res,next) => {
   next();
 });
 
+app.get("/", (req,res) => {
+  req.redirect("/listings");
+})
+
 
 app.use("/listings", listingsRouter);
 app.use("/listings/:id/reviews",reviewsRouter);
